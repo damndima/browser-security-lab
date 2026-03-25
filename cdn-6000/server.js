69ca1ab88357f40196d45240b7d91980824576c1
dimastrakhov <22fmif.d.strahov@std.npu.edu.ua>
2026-03-25 15:35:12 +0000

    Lab 3 (Tasks 1 & 2): Simulated CDN breach and implemented Subresource Integrity (SRI) defense

--- cdn-6000/server.js
+++ cdn-6000/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
-const port = 6001;
+const port = 6001; 
+
+app.use(cors());
+
+const isBreachMode = process.argv.includes('--mode') && process.argv.includes('breach');
+
+app.get('/react-mock.js', (req, res) => {
+    res.setHeader('Content-Type', 'application/javascript');
+    
+    if (isBreachMode) {
+        res.send('alert("CRITICAL: CDN Compromised! Stealing data...");');
+    } else {
+        res.send('console.log("React v1.0.0 loaded from CDN (Port 6000)");');
+    }
+});
 
 app.use(express.static(__dirname));
 
 app.listen(port, () => {
-  console.log(`CDN (StaticHost) is running on http://localhost:${port}`);
+    console.log(`CDN (StaticHost) is running on http://localhost:${port}! Breach Mode: ${isBreachMode}`);
 });
